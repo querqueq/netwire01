52d7dcb601c03554e121a1cd5335753ddc280f25
1225449 Johannes <e1225449@student.tuwien.ac.at>
2017-02-23 05:56:26 +0100

    Init

---
+++ app/Main.hs
@@ -0,0 +1,227 @@
+{-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE FlexibleContexts #-}
+
+module Main where
+
+import Prelude hiding ((.)) -- To use (.) in the scope of Categories instead
+import qualified Prelude as P ((.))
+import Control.Wire 
+import qualified Graphics.Rendering.OpenGL as GL
+import qualified Graphics.UI.GLFW as GLFW
+import FRP.Netwire 
+import Data.IORef
+import Data.Fixed (mod')
+import Debug.Trace
+import Control.Monad
+import Control.Monad.IO.Class
+import Data.IORef
+import Data.StateVar (($=))
+--import qualified Physics.Hipmunk as H
+import FRP.Netwire.Input
+import FRP.Netwire.Input.GLFW
+import qualified Graphics.Rendering.FTGL as FTGL
+import Numeric 
+import System.Random
+import Stars
+
+data GameState = GameState
+    { playerShip    :: Ship
+    , stars         :: [(Float,Float)]
+    } deriving Show
+
+data Ship = Ship
+    { posX      :: Float
+    , posY      :: Float
+    , aX        :: Float
+    , aY        :: Float
+    , vX        :: Float
+    , vY        :: Float
+    , aR        :: Float
+    , vR        :: Float
+    , dR        :: Float
+    , thrusters :: Thrusters
+    } deriving Show
+
+data Thrusters = Thrusters
+    { thrusterFront     :: Thruster
+    , thrusterBack      :: Thruster
+    , thrusterLeft      :: Thruster
+    , thrusterRight     :: Thruster
+--    , thrusterRLeft     :: Thruster
+--    , thrusterRRight    :: Thruster
+    } deriving Show
+
+data Thruster = Thruster
+    { thrusterOffsetX   :: Float
+    , thrusterOffsetY   :: Float
+    , thrusterOffsetR   :: Float
+    , thrusterThrust    :: Float
+    } deriving Show
+
+prettyShow :: Ship -> String
+prettyShow (Ship {..}) = foldr (\(name,val,unit) xs -> name ++ ": " ++ format val ++ unit ++ " " ++ xs) "" 
+    [("X", posX, "")
+    ,("Y", posY, "")
+    ,("Rotation", dR * 180 / pi, "°")
+    ,("Y-Thrust", aY, "")
+    ,("X-Thrust", aX, "")
+    ,("R-Thrust", aR, "")
+    ]
+    where format x = showFFloat (Just 2) x ""
+
+type InputWire s a b = Wire s () (GLFWInputT IO) a b
+
+thrust :: [GLFW.Key] -> Float -> InputWire s () Float
+thrust ks a = pure a . (foldr (<|>) (keyPressed GLFW.Key'X) $ map keyPressed ks) <|> pure 0
+
+f :: Float
+f = 1.5
+
+s :: Float
+s = 0.02 / f
+
+mainAcceleration :: Float
+mainAcceleration = 0.2 / f
+
+maneuveringAcceleration :: Float
+maneuveringAcceleration = 0.1 / f
+
+rotationalAcceleration :: Float
+rotationalAcceleration = 2
+
+frontThrust         = thrust [GLFW.Key'W,GLFW.Key'Up] mainAcceleration
+leftThrust          = thrust [GLFW.Key'A,GLFW.Key'Left] (-maneuveringAcceleration)
+backThrust          = thrust [GLFW.Key'S,GLFW.Key'Down] (-maneuveringAcceleration)
+rightThrust         = thrust [GLFW.Key'D,GLFW.Key'Right] maneuveringAcceleration
+rotateLeftThrust    = thrust [GLFW.Key'Q] rotationalAcceleration
+rotateRightThrust   = thrust [GLFW.Key'E] (-rotationalAcceleration)
+
+title :: String
+title = "Netwire 01"
+
+shipWire :: (HasTime t s) => InputWire s () Ship
+shipWire = Ship
+       <$> (fst <$> pos)
+       <*> (snd <$> pos)
+       <*> xaccel'
+       <*> yaccel'
+       <*> xspeed'
+       <*> yspeed'
+       <*> raccel'
+       <*> rspeed'
+       <*> rdegree'
+       <*> (Thrusters <$> (Thruster <$> pure 0 <*> pure s <*> pure pi <*> frontThrust)
+                      <*> (Thruster <$> pure 0 <*> pure (-s) <*> pure (-pi) <*> backThrust)
+                      <*> (Thruster <$> pure 0 <*> pure (-s) <*> pure (pi/2) <*> leftThrust)
+                      <*> (Thruster <$> pure 0 <*> pure s <*> pure (-3*pi/2) <*> rightThrust)
+           )
+
+renderThrust :: (Float,Float) -> Float -> Thruster -> IO ()
+renderThrust (x,y) r (Thruster {..}) = if thrusterThrust /= 0 
+    then GL.renderPrimitive GL.Triangles
+            $ mapM_ renderPoint 
+            $ map (rotatePoint (x,y) (r+thrusterOffsetR)) 
+              [(x',y'),(x'-s'/2,y'+s'),(x'+s'/2,y'+s')]
+    else return ()
+    where x' = x + thrusterOffsetX 
+          y' = y + thrusterOffsetY
+          s' = thrusterThrust / 5
+
+renderPoint :: (Float, Float) -> IO ()
+renderPoint (x, y) = GL.vertex $ GL.Vertex2 (realToFrac x :: GL.GLfloat)
+                                            (realToFrac y :: GL.GLfloat)
+generatePoints :: Float -> Float -> Float -> [(Float, Float)]
+generatePoints x y s =
+    [ (x - s, y - s)
+    , (x + s, y - s)
+    , (x + s2, y + s)
+    , (x - s2, y + s)
+    ] where s2 = s / 4
+
+run :: FTGL.Font -> GLFW.Window -> GLFWInputControl -> IO ()
+run font window inptCtrl = do
+        inpt <- getInput inptCtrl
+--        xys <- zip <$> f <*> f
+--        let ss = map (\(x,y) -> Star x y 0 1) xys
+        g <- getStdGen
+        runNetwork inpt clockSession_ shipWire $ skyWire g 678
+    where 
+        runNetwork inpt session wire sky = do
+            --GLFW.pollEvents
+            inpt' <- pollGLFW inpt inptCtrl
+            (st , session') <- stepSession session
+            ((wt', wire'), inpt'') <-
+                runGLFWInputT (stepWire wire st $ Right undefined) inpt'
+            (stars, sky') <- stepWire sky st $ Right undefined
+            shouldClose <- GLFW.windowShouldClose window
+            if shouldClose
+            then return ()
+            else case wt' of
+                Left _ -> return ()
+                Right ship@(Ship {..}) -> do
+                    GL.clearColor GL.$= GL.Color4 0.0 0.0 0.0 1
+                    GL.clear [GL.ColorBuffer]
+                    FTGL.renderFont font (prettyShow ship) FTGL.Front
+                    case stars of 
+                        Left _ -> return ()
+                        Right stars -> mapM_ renderStar stars
+                    GL.renderPrimitive GL.Quads 
+                        $ mapM_ renderPoint 
+                        $ map (rotatePoint (posX,posY) dR)
+                        $ generatePoints posX posY s
+                    renderThrust (posX, posY) dR (thrusterFront thrusters)
+                    renderThrust (posX, posY) dR (thrusterBack thrusters)
+                    renderThrust (posX, posY) dR (thrusterLeft thrusters)
+                    renderThrust (posX, posY) dR (thrusterRight thrusters)
+                    GL.flush
+                    GLFW.swapBuffers window
+                    runNetwork inpt'' session' wire' sky'
+
+xaccel' :: InputWire s () Float
+xaccel' = (+) <$> leftThrust <*> rightThrust
+
+xspeed' :: HasTime t s => InputWire s () Float
+xspeed' = integral 0 . (fst <$> rxyaccel)
+
+yaccel' :: InputWire s () Float
+yaccel' = (+) <$> frontThrust <*> backThrust
+
+yspeed' :: HasTime t s => InputWire s () Float
+yspeed' = integral 0 . (snd <$> rxyaccel)
+
+raccel' :: InputWire s () Float
+raccel' = (+) <$> rotateLeftThrust <*> rotateRightThrust
+
+rspeed' :: HasTime t s => InputWire s () Float
+rspeed' = integral 0 . raccel'
+
+rdegree' :: HasTime t s => InputWire s () Float
+rdegree' = (`mod'` (2*pi)) <$> integral 0 . raccel'
+
+--negateInertia :: Monoid e => Wire s e IO () Float
+
+pos :: HasTime t s => InputWire s () (Float, Float)
+pos = (,) <$> integral 0 . (fst <$> rspeeds') <*> integral 0 . (snd <$> rspeeds')
+
+rxyaccel :: HasTime t s => InputWire s () (Float, Float)
+rxyaccel = (\d (aX,aY) -> rotatePoint (0,0) d (aX,aY)) <$> rdegree' <*> ((,) <$> xaccel' <*> yaccel')
+
+rspeeds' :: HasTime t s => InputWire s () (Float,Float)
+rspeeds' = (,) <$> xspeed' <*> yspeed'
+
+rotatePoint :: (Float, Float) -> Float -> (Float, Float) -> (Float, Float)
+rotatePoint (xo,yo) θ (x,y) = (x' * (cos θ) - y' * (sin θ) + xo
+                              ,y' * (cos θ) + x' * (sin θ) + yo)
+    where x' = x - xo
+          y' = y - yo
+
+main = do
+    GLFW.init
+    (Just window) <- GLFW.createWindow 1080 1080 title Nothing Nothing
+    GLFW.makeContextCurrent (Just window)
+    font <- FTGL.createBitmapFont "DroidSansMono.ttf"
+    FTGL.setFontFaceSize font 24 72
+    mkInputControl window >>= run font window
+    traceM "Destroying"
+    GLFW.destroyWindow window
+    GLFW.terminate
---
+++ src/Stars.hs
@@ -0,0 +1,109 @@
+{-# LANGUAGE RecordWildCards #-}
+
+module Stars (skyWire,renderStar) where
+
+import Prelude hiding ((.)) -- To use (.) in the scope of Categories instead
+import Control.Wire 
+import FRP.Netwire 
+import qualified Graphics.Rendering.OpenGL as GL
+import qualified Graphics.UI.GLFW as GLFW
+import qualified Graphics.Rendering.FTGL as FTGL
+import System.Random
+import Control.Monad
+import Numeric 
+
+type Point = (Float,Float)
+data Star = Star 
+    { starX :: Float
+    , starY :: Float
+    , starZ :: Float
+    , starBrightness :: Float
+    } 
+
+instance Show Star where
+    show (Star {..}) = "(" ++ format starX ++ "," ++ format starY ++ ") " ++ format starBrightness ++ "*"
+
+format x = showFFloat (Just 2) x ""
+
+skyWire :: (Fractional t, Monad m, HasTime t s, RandomGen g) => g -> Int -> Wire s () m a [Star]
+skyWire g starsN = do
+    let xys = zip (take starsN rands) (drop starsN rands)
+    let gs = map mkStdGen $ randoms g
+    sequenceA $ zipWith pulsarWire gs xys
+    where rands = take (starsN * 2) $ randomRs (-1 :: Float,1) g
+
+starPosWire :: Point -> Wire s e m a Point
+starPosWire pos = WConst $ Right pos
+
+randStarPosWire :: (RandomGen g) => g -> Wire s e m a Point
+randStarPosWire g = do
+    let (x:(y:_)) = randoms g
+    starPosWire (x,y)
+
+pulsarWire :: (Monad m, RandomGen g, HasTime t s, Fractional t) => g -> Point -> Wire s () m a Star 
+pulsarWire g (x,y) = Star
+         <$> pure x
+         <*> pure y
+         <*> pure 0
+         <*> alternateByInhibit (wackelkontakt 0.4137 0.992 g . pure base) (pulsateWire base peak pulsateT 0.5) 
+         where base = fst $ randomR (0.001,0.002) g
+               peak = fst $ randomR (0.005,0.025) g -- 0.01
+               pulsateT = 2.5
+
+pulsateWire :: (Fractional t, Monad m, HasTime t s) => Float -> Float -> t -> t -> Wire s () m a Float
+pulsateWire base peak totalT holdT = for totalT . integral base .
+    (   for changeT . integral target . pure (-target)
+    --> for holdT . pure 0 
+    --> for changeT . integral (-target) . pure target 
+    --> pure 0
+    )
+    where changeT = (totalT - holdT) / 2
+          target = peak - base
+
+alternateByInhibit w1 w2 = w1 --> w2 --> alternateByInhibit w1 w2
+
+renderPoint :: Point -> IO ()
+renderPoint (x, y) = GL.vertex $ GL.Vertex2 (realToFrac x :: GL.GLfloat) (realToFrac y :: GL.GLfloat)
+
+renderStar :: Star -> IO () 
+renderStar (Star {..}) = GL.renderPrimitive GL.Lines
+    $ mapM_ renderPoint 
+    $ [(x-b,y),(x+b,y)
+      ,(x,y-b),(x,y+b)
+      ,(x-b',y+b'),(x+b',y-b')
+      ,(x+b',y+b'),(x-b',y-b')
+      ]
+    where x = starX; y = starY; b = starBrightness; b' = starBrightness / 3
+
+--{--
+runStars :: GLFW.Window -> IO ()
+runStars window = do
+    g <- getStdGen
+    runNetwork clockSession_ $ skyWire g 678
+    where runNetwork sess wire = do
+            (st,sess') <- stepSession sess
+            (stars,wire') <- stepWire wire st $ Right undefined
+            shouldClose <- GLFW.windowShouldClose window
+            if shouldClose
+            then return ()
+            else case stars of
+                Left _ -> return ()
+                Right stars -> do
+                    GL.clearColor GL.$= GL.Color4 0.0 0.0 0.0 1
+                    GL.clear [GL.ColorBuffer]
+                    mapM_ renderStar stars
+                    GL.flush
+                    GLFW.swapBuffers window
+                    runNetwork sess' wire'
+--}
+
+testStars :: IO ()
+testStars = do
+    GLFW.init
+    (Just window) <- GLFW.createWindow 1280 1280 "Stars Demo" Nothing Nothing
+    GLFW.makeContextCurrent (Just window)
+    font <- FTGL.createBitmapFont "DroidSansMono.ttf"
+    FTGL.setFontFaceSize font 24 72
+    runStars window
+    GLFW.destroyWindow window
+    GLFW.terminate
